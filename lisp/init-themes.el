01f61f34cfc91fc57fe7867618ee855bead37232
eshion <eshion@qq.com>
2014-06-10 12:57:11 +0800

    init-themes

---
+++ lisp/init-themes.el
@@ -0,0 +1,18 @@
+(when (< emacs-major-version 24)
+  (require-package 'color-theme))
+
+(require-package 'monokai-theme)
+
+(setq-default custom-enabled-themes '(monokai))
+
+;; Ensure that themes will be applied even if they have not been customized
+(defun reapply-themes ()
+  "Forcibly load the themes listed in `custom-enabled-themes'."
+  (dolist (theme custom-enabled-themes)
+    (unless (custom-theme-p theme)
+      (load-theme theme)))
+  (custom-set-variables `(custom-enabled-themes (quote ,custom-enabled-themes))))
+
+(add-hook 'after-init-hook 'reapply-themes)
+
+(provide 'init-themes)
