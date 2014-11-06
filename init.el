a9932d0f868c93a0f67a67dddf575f72a64931d1
eshion <eshion@qq.com>
2014-11-06 17:42:13 +0800

    Update init.el

--- init.el
+++ init.el
@@ -131,7 +131,6 @@
 ;;----------------------------------------------------------------------------
 ;; Locales (setting them earlier in this file doesn't work in X)
 ;;----------------------------------------------------------------------------
-(require 'init-locales)
 (require 'init-unicad)
 (add-hook 'after-init-hook
           (lambda ()
